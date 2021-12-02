a7ba817b2314b1b5e2087ac6cefa07d9f57837b3
sanyaburba <alexandroff.ok@gmail.com>
2021-12-02 15:11:27 +0300

    topic 2

--- topic_1.js
+++ topic_1.js
@@ -8,10 +8,10 @@
 //** Array: Complete the method which accepts such an array, and returns that single different number (length >= 3) */ 
 //* Task 2
 
-// let arr = [9, 5, 9, 9, 9];
-// let anotherNumber = numbers => numbers.reduce((x, y) => x ^ y);
-// let result = anotherNumber(arr);
-// alert(result);
+let arr = [9, 5, 9];
+let anotherNumber = numbers => numbers.reduce((x, y) => x ^ y);
+let result = anotherNumber(arr);
+console.log(result);
 
 //** Array: Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number. */ 
 //* Task 3
@@ -36,67 +36,65 @@
 
 // console.log(deepCount(arr));
 
-//** Array: The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars. Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line. Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue? */ 
-//* Task 5
+//! Array: The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars. Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line. Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue? */ 
+//! Task 5
 
-// let money = 0;
-// let queue = [25, 25, 25, 100, 100];
+let queue = [25, 25, 25, 50, 50];
+let money = 0;
 
-// queue.forEach((item) => {
-//    if (item == 25) {
-//       money += 25;
-//    } else if (item == 50) {
-//       money -= 25;
-//    } else if (item == 100) {
-//       money -= 75;
-//    }
-//    console.log(money);
-//    switch (money < 0) {
-//       case true: {
-//          console.log('he cant');
-//          break;
-//       }
-//       case false: {
-//          console.log('he can');
-//          break;
-//       }
-//    }
-// })
+function sum(arr) {
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == 25) {
+         money += 25;
+      } else if (arr[i] == 50) {
+         money -= 25
+      } else if (arr[i] == 100) {
+         money -= 75
+      }
+      if (money > !0) {
+         console.log('he cant');
+         break
+      }
+
+   }
+
+   sum(queue)
 
 
-//** Write an if condition to check that age is NOT between 14 and 90 inclusively. */
-//* Task 6
 
-// let age = 100
+   //** Write an if condition to check that age is NOT between 14 and 90 inclusively. */
+   //* Task 6
 
-// if (age >= 14 && age <=90) {
-//    console.log('no, age is between 14 and 90');
-// } else {
-//    console.log('age is out of 14 and 90');
-// }
+   // let age = 100
+
+   // if (age >= 14 && age <=90) {
+   //    console.log('no, age is between 14 and 90');
+   // } else {
+   //    console.log('age is out of 14 and 90');
+   // }
 
 
-//** Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).*/
-//* Task 7
+   //** Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).*/
+   //* Task 7
 
-// let x = 0
-// while( x < 3) console.log( `number ${x++}!` );
+   // let x = 0
+   // while( x < 3) console.log( `number ${x++}!` );
 
-//** Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again. The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line. */ 
-//* Task 8
+   //** Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again. The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line. */ 
+   //* Task 8
 
-// do {
-//  let num = prompt("Please type number greater than 100", 0);
-// } while (num <= 100 && num);
+   // do {
+   //  let num = prompt("Please type number greater than 100", 0);
+   // } while (num <= 100 && num);
 
-//** Write a function min(a,b) which returns the least of two numbers a and b */
-//* Task 9
+   //** Write a function min(a,b) which returns the least of two numbers a and b */
+   //* Task 9
 
-// let min = (a,b) => console.log(a < b ? a : b);
-// min(45,2)
+   // let min = (a,b) => console.log(a < b ? a : b);
+   // min(45,2)
 
-//** Rewrite the function using '?' and '||'. */
-//* Task 10
+   //** Rewrite the function using '?' and '||'. */
+   //* Task 10
 
-// let checkAgeConditional = age => age > 18 ? true : confirm('did parents allow you');
-// let checkAgeLogical = age => age > 18 || confirm('did parents allow you');
+   // let checkAgeConditional = age => age > 18 ? true : confirm('did parents allow you');
+   // let checkAgeLogical = age => age > 18 || confirm('did parents allow you');
